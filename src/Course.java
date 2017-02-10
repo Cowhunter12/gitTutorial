e8a5c4d841f6e82cbb76f3fc5a1ef0f2c842663d
steven <scrossma@uwo.ca>
2017-02-10 11:26:50 -0500

    Feature 1 first commit

---
+++ src/Course.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+/**
+ * Created by steven on 2/10/2017.
+ */
+public class Course {
+    List<Course> courseList;
+    public void addCourse(Course c){
+        courseList.add(c);
+    }
+}
